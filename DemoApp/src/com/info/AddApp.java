05622c257fa595ffb3023fd0ff96eca03ee20616
lkgit999 <lkgit999@gmail.com>
2022-12-22 15:29:47 -0500

    second commit

--- DemoApp/src/com/info/AddApp.java
+++ DemoApp/src/com/info/AddApp.java
@@ -4,7 +4,9 @@ public class AddApp {
 
 	public static void main(String[] args) {
 		int a=5 , b=30;
+		System.out.println("Test");
 		System.out.println("a+b ="+(a+b));
+		System.out.println("Addition");
 
 	}
 
